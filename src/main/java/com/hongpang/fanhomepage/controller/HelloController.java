457c4165c76b4aed085aebb64683cd8aa5fa39b9
ChanYiHong <chanyinara@naver.com>
2021-02-16 20:56:27 +0900

    branch test

--- src/main/java/com/hongpang/fanhomepage/controller/HelloController.java
+++ src/main/java/com/hongpang/fanhomepage/controller/HelloController.java
@@ -1,4 +1,14 @@
 package com.hongpang.fanhomepage.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hong & Pang first project!!";
+    }
+
 }
