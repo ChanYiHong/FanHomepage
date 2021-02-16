41a2379cf667023af3f2a34d3467e922472add41
ChanYiHong <chanyinara@naver.com>
2021-02-16 20:06:09 +0900

    Initial commit

---
+++ src/main/java/com/hongpang/fanhomepage/FanHomepageApplication.java
@@ -0,0 +1,13 @@
+package com.hongpang.fanhomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanHomepageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongpang/fanhomepage/FanHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongpang.fanhomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
