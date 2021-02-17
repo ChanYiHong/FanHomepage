ae4097af0eb165305064c8c3193ea4e05fcb8fd3
ChanYiHong <chanyinara@naver.com>
2021-02-17 22:49:55 +0900

    index controller

--- src/main/java/com/hongpang/fanhomepage/controller/HelloController.java
+++ src/main/java/com/hongpang/fanhomepage/controller/HelloController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hong & Pang first project!!";
+    public String index() {
+        return "/index";
     }
 
 }
